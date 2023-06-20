3d91827e85dc1f4724ffb04159d3e9b295d8c0ef
raunaksingh1806 <55934288+raunaksingh1806@users.noreply.github.com>
2023-06-20 19:21:26 -0300

    add nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FAQ from "./components/FAQ";
 import ContactPage from "./pages/ContactPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <NavBar></NavBar>
+      </header>
       <Router>
         <Routes>
           <Route path="/faq" element={<FAQ />} />
