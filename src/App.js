f8e7e76636eb7c686684759f379870dcd032d328
raunaksingh1806 <55934288+raunaksingh1806@users.noreply.github.com>
2023-06-20 18:51:53 -0300

    remove default react page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FAQ from './components/FAQ'
-import ContactPage from './pages/ContactPage';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FAQ from "./components/FAQ";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <div className="App">
+      <header></header>
       <Router>
         <Routes>
-          <Route path="/faq" element={<FAQ/>}/>
+          <Route path="/faq" element={<FAQ />} />
 
-          <Route path="/contact" element={<ContactPage/>}/>
-
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
