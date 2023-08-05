667554437cbcbcac3cd1b519c1c8f057de9ba5f6
Ammar <ammar.k@dal.ca>
2023-08-05 17:27:03 -0300

    Modified incorrect sql statement

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.listen(port, () => {
 
 app.get('/user', (req, res) => {
   const userID = req.query.id;
-  const sql = 'SELECT FirstName, LastName, Email, Password, Address, DateJoined FROM Users WHERE UserID = ?';
+  const sql = 'SELECT FirstName, LastName, Email, Password, DateJoined FROM Users WHERE UserID = ?';
   conn.query(sql, [userID], (err, result) => {
       if(err) { 
         throw err;
@@ -43,16 +43,16 @@ app.get('/user', (req, res) => {
         res.status(404).send('User not found');
       }
       else {
-        res.json({firstName: result[0].FirstName, lastName: result[0].LastName, email: result[0].Email, address: result[0].Address, dateJoined: result[0].DateJoined, password: result[0].Password});
+        res.json({firstName: result[0].FirstName, lastName: result[0].LastName, email: result[0].Email, dateJoined: result[0].DateJoined, password: result[0].Password});
       }
     })
 });
 
 app.post('/userUpdate', (req, res) => {
   const userID = req.query.id;
-  const sql = 'UPDATE Users SET FirstName = ?, LastName = ?, Email = ?, Password = ?, Address = ? WHERE UserID = ?';
-  const {firstName, lastName, email, password, address} = req.body;
-  conn.query(sql, [firstName, lastName, email, password, address, userID], (err) => {
+  const sql = 'UPDATE Users SET FirstName = ?, LastName = ?, Email = ?, Password = ? WHERE UserID = ?';
+  const {firstName, lastName, email, password} = req.body;
+  conn.query(sql, [firstName, lastName, email, password, userID], (err) => {
     if(err) {
       throw err;
     }
