0d6295a51a1daed7d0b611317e5600ce92e5065b
raunaksingh1806 <55934288+raunaksingh1806@users.noreply.github.com>
2023-08-07 12:00:29 -0300

    add styling for blogs page

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -3,15 +3,18 @@ import { useNavigate } from "react-router-dom";
 import AllBlogs from "../components/AllBlogs";
 
 function Blogs() {
-
     const navigate = useNavigate();
 
     const handleWriteBlog = () => {
-        navigate('/writeBlog');
-    }
+        navigate("/writeBlog");
+    };
 
     return (
         <div className="main-blogs-page">
+            <div className="main-blogs-form-heading">Maintenance Blogs</div>
+            <div className="main-blogs-form-subheading">
+                Ready to Share Your Car Insights? Write a Maintenance Blog!
+            </div>
             <button onClick={handleWriteBlog}>Write a Blog</button>
 
             <AllBlogs></AllBlogs>
@@ -19,4 +22,4 @@ function Blogs() {
     );
 }
 
-export default Blogs
+export default Blogs;
