14cc7f9640bd8d8b0c36758905e617b45ac85607
Raunak Singh <raunak.singh@dal.ca>
2023-08-12 00:30:54 -0300

    Add Login Check: Vendors Page (#45)
    * add login check, vendor page
    
    * update header
    
    ---------
    
    Co-authored-by: raunaksingh1806 <55934288+raunaksingh1806@users.noreply.github.com>

--- src/pages/Vendors.js
+++ src/pages/Vendors.js
@@ -1,9 +1,10 @@
-// Created by 
+// Created by
 // SAMEER MOHAMED, B00845973
+// Co-Authored: Raunak Singh, B00831843
 
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import "./css/vendor.css";
 import orangeCar from "./images/carwash4-unsplash.jpg";
 import redCar from "./images/carwash6-unsplash.jpg";
@@ -16,44 +17,58 @@ const Vendor = () => {
     const [vendors, setVendors] = useState([]);
     const [shuffledImages, setShuffledImages] = useState([]);
 
+    const loggedUserId = localStorage.getItem("userID");
+
     useEffect(() => {
         fetchVendors();
     }, []);
 
     useEffect(() => {
-        setShuffledImages(shuffleArray([
-            blackBMW,
-            orangeCar,
-            soapCar,
-            redCar,
-            washingPorche,
-            blackPorsche,
-        ]));
+        setShuffledImages(
+            shuffleArray([
+                blackBMW,
+                orangeCar,
+                soapCar,
+                redCar,
+                washingPorche,
+                blackPorsche,
+            ])
+        );
     }, []);
 
     const fetchVendors = () => {
-        axios.get('https://csci-4177-grp-21.onrender.com/vendors')
+        axios
+            .get("https://csci-4177-grp-21.onrender.com/vendors")
             .then((response) => setVendors(response.data))
             .catch((error) => console.error("Error fetching vendors:", error));
     };
 
     const shuffleArray = (array) => {
-        let currentIndex = array.length, randomIndex;
+        let currentIndex = array.length,
+            randomIndex;
         while (currentIndex !== 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
         }
         return array;
-    }
+    };
 
     const handleVendorClick = (vendorId) => {
-        sessionStorage.setItem("selectedVendorId", vendorId);
+        if (loggedUserId !== null) {
+            sessionStorage.setItem("selectedVendorId", vendorId);
+        } else {
+            alert(
+                "Please log in with your registered account before accessing Vendor service booking."
+            );
+        }
     };
 
     return (
         <div className="vendor-container">
-
             <div className="vendors-list-heading">Vendors List</div>
 
             <div className="vendors-list-subheading">
@@ -63,7 +78,11 @@ const Vendor = () => {
             {vendors.map((vendor, index) => (
                 <Link
                     key={vendor.vendor_id}
-                    to={`/vendors/${vendor.vendor_id}`}
+                    to={
+                        localStorage.getItem("userID")
+                            ? `/vendors/${vendor.vendor_id}`
+                            : "#"
+                    }
                     onClick={() => handleVendorClick(vendor.vendor_id)}
                 >
                     <div className="vendor-card">
